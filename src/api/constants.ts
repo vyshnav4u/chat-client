b0d75fa643b7a18d7eff705bce596f7bc69ae3b1
YsH <mk.vyshnav1996@gmail.com>
2025-04-03 22:49:09 +0530

    uri updated

--- src/api/constants.ts
+++ src/api/constants.ts
@@ -1,3 +1,3 @@
-const SERVER_URI = import.meta.env.VITE_SERVER_URI;
+export const SERVER_URI = import.meta.env.VITE_SERVER_URI;
 
 export const MESSAGE_URI = `${SERVER_URI}/api/v1/messages`;
--- src/http/connectSocket.ts
+++ src/http/connectSocket.ts
@@ -1,7 +1,8 @@
 import { io } from 'socket.io-client';
+import { SERVER_URI } from '../api/constants';
 
 export const initSocketConnection = () => {
-	const SERVER_PORT = 'http://localhost:8000';
+	const SERVER_PORT = SERVER_URI;
 	const socket = io(SERVER_PORT);
 	return socket;
 };
